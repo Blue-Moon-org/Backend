fa5d6b322b6fe7e905e9bbb73b61d9e6103170ba
hakeem750 <hakeemy056@gmail.com>
2023-12-22 12:17:05 +0100

    admin-add

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,57 @@ from django.contrib import admin
 from .models import User, Feedback, Report
 
 
-admin.site.register(User)
-admin.site.register(Feedback)
-admin.site.register(Report)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "title",
+        "text",
+        "user",
+        "created_on",
+    )
+    list_filter = (
+        "user",
+        
+    )
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "account_type",
+        "email",
+        "phone",
+        "brand_name",
+        "firstname",
+        "lastname",
+        "sex",
+        "email_verified",
+        "phone_verified",
+        "is_active",
+        "created_at"
+    )
+    list_filter = (
+        "account_type",
+        "sex",
+        "email_verified",
+        "phone_verified",
+        "is_active",
+        
+    )
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "recipient",
+        "title",
+        "text",
+        "user",
+        "created_on",
+    )
+    list_filter = (
+        "user",
+        
+    )
+admin.site.register(User, UserAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(Report, ReportAdmin)
